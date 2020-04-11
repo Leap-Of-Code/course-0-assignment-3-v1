280eef1eb587b2eac05142b067a31a61e54bf7e9
nicoleyip1876 <nicoleyip1876@yahoo.com>
2020-04-11 05:01:39 +0000

    Updating all questions

--- fix-code-3/main.cpp
+++ fix-code-3/main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main() {
   const unsigned int jim_age = 32;
   const unsigned int daphny_age = 28;
-  const unsigned int agg_difference = jim_age - daphny_age;
-  cout << "Daphny is " << age_difference << " years older then Jim.";
+  const unsigned int age_difference = jim_age - daphny_age;
+  cout << "Daphny is " << age_difference << " years younger then Jim.";
 }
