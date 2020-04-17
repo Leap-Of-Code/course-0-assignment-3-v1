86c8d55b9c63770dd6697e314725f00d3bd87b1e
jinghe315 <63163103+jinghe315@users.noreply.github.com>
2020-04-17 07:26:51 +0000

    Updating all questions

--- invent-code-0/main.cpp
+++ invent-code-0/main.cpp
@@ -3,18 +3,21 @@
 using namespace std;
 
 int main() {
-  int original;
+  float original;
   float original_float;
   original_float = 13.5;
-  cout << "Please enter a integer: ";
+  cout << "Please enter a number: ";
   cin >> original;
-  int clone = original + 14;
-  int product = original * clone;
+  float clone = original + 14;
+  float product = original * clone;
   cout << "The product is: " << product;
 }
 
 /*
-Type   | Name     | Value   | Size
------- | -------- | ------- | ------
-       |          |         |
+Type   | Name          | Value            | Size
+------ | --------      | -------          | ------
+  int  | original      | cin              | 4 bytes
+  float| original float| 13.5             | 4 bytes
+  float| clone         | cin + 14         | 4 bytes
+  float| product       | cin * (cin + 14) | 4 bytes 
 */
--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -3,5 +3,21 @@
 using namespace std;
 
 int main() {
-  // Write code here.
+int number1; 
+int number2;
+cout << "please enter integer #1: " << endl; 
+cin >> number1;
+cout << "please enter integer #2: " << endl;
+cin >> number2;
+
+int sum = number1 + number2;
+int product = number1 * number2;
+float quotient = number1 / number2;
+int difference = number1 - number2;
+
+cout << "Sum of the 2 numbers: " << sum << endl; 
+cout << "Product of the 2 numbers: " << product << endl;
+cout << "Quotient of the 2 numbers: " << quotient << endl;
+cout << "Difference between the 2 numbers: " << difference << endl; 
+
 }
