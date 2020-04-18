8475b1ae505a2a349c9aeafe95fe0cb15f7fccfb
john-lakeman <60250176+john-lakeman@users.noreply.github.com>
2020-04-18 05:26:39 +0000

    Updating all questions

--- fix-code-2/main.cpp
+++ fix-code-2/main.cpp
@@ -7,5 +7,5 @@ int main() {
   const float gold_fish_tank_size_liters = 10.4;
   string const gold_fish_name = "carl";
 
-  cout << "I own a goldfish named " << gold_fish_name << ". his tank is " << gold_fish_tank_size_liters << " liters big, and he eats " << gold_fish_food_weight_lbs << "pounds of food.";
+  cout << "I own a goldfish named " << gold_fish_name << ". His tank is " << gold_fish_tank_size_liters << " liters big, and he eats " << gold_fish_food_weight_lbs << " pounds of food." << endl;
 }
