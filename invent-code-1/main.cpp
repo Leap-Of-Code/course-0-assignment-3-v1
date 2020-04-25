0799a7decb92b63e9f7e3bf1219f875d8580ff88
gsheren1210 <63162654+gsheren1210@users.noreply.github.com>
2020-04-25 04:07:33 +0000

    Updating all questions

--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -4,4 +4,21 @@ using namespace std;
 
 int main() {
   // Write code here.
+  int interger1;
+  int interger2;
+
+  cout << "Write two intergers to compute :" << endl;
+  cin >> interger1;
+  cout << "and ";
+  cin >> interger2;
+
+  int product = interger1 * interger2;
+  int sum = interger1 + interger2;
+  int quotient = interger1 / interger2;
+  int difference = interger1 - interger2;
+
+  cout << "The sum is " << sum << endl;
+  cout << "The product is " << product << endl;
+  cout << "The quotient is " << quotient << endl;
+  cout << "The difference is " << difference << endl;
 }
