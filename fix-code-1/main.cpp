21f7d8bf3b23840f37a782e52cb53491733974d4
Harshelite2503 <67180856+Harshelite2503@users.noreply.github.com>
2020-06-21 12:59:37 +0000

    Saving progress for all questions.

--- fix-code-1/main.cpp
+++ fix-code-1/main.cpp
@@ -9,8 +9,8 @@ int main() {
   const float food_per_day_cost = 21.6;
   const float num_days = 8;
 
-  const float trip_cost = flights_cost * num_flights + food_per_day_cost * num_days;
+   float trip_cost = flights_cost * num_flights + food_per_day_cost * num_days;
   const float discount_flight_voucher = 15.7;
-  trip_cost = trip_cost - flight_voucher;
+  trip_cost = trip_cost - discount_flight_voucher;
   cout << "The total cost of the trip including the vouchers will be $" << trip_cost << endl;
 }
