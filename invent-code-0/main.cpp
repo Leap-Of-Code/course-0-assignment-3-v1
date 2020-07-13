04bf8c0d7505bd04a73e33014b751d39360833bc
Harshelite2503 <67180856+Harshelite2503@users.noreply.github.com>
2020-07-13 14:45:32 +0000

    Updating all questions

--- invent-code-0/main.cpp
+++ invent-code-0/main.cpp
@@ -3,14 +3,17 @@
 using namespace std;
 
 int main() {
-  int original;
-  float original_float;
-  original_float = 13.5;
+   int original;
+  const float original_float= 13.5;
   cout << "Please enter a integer: ";
   cin >> original;
   int clone = original + 14;
   int product = original * clone;
-  cout << "The product is: " << product;
+  cout << "Type" << "    |    Name    "  << "    |  Value" << "     |  Size" << endl;
+  cout << "Integer" << " |   original     |    " <<   original << "     | 4 bytes" <<endl;
+  cout << "Float" << "   | original_float |    " <<   original_float << "    | 4 bytes" << endl;
+  cout << "Integer" << " |    clone       |    " <<   clone << "     | 4 bytes" <<endl;
+  cout << "Integer" << " |    product     |    " <<   product << "  | 4 bytes" <<endl;
 }
 
 /*
